0d4732744a7de9eee2526acc6a932b151368f901
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-25 22:39:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ function moveIt() {
         pix -= 5;*/
     el.style.marginLeft = pix +'px';
 }
-el.onClick = function () {
+el.onclick = function () {
     var interval = setInterval(moveIt,50);
 }
