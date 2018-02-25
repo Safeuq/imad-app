ff4fa88e629552fcb3aba2b4d6ff5d6adcfac3ae
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-25 22:09:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var el = document.getElementById('madi');
+var pix= 0;
+function moveIt() {
+    pix = pix + 5;
+    el.style.marginLeft = pix +'px';
+}
+el.onClick = function() {
+    var int = setInterval(moveIt,1);
+}
