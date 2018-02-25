0b1c8dc684698357c4e54657f2f11c669677bf49
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-25 22:19:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,14 @@ var el = document.getElementById('madi');
 var pix= 0;
 var state=true;
 function moveIt() {
-    if(pix===60)
+    /*if(pix===60)
         state=false;
     if(pix===-60)
         state=true;
-    if(state)
+    if(state)*/
         pix += 5;
-    else
-        pix -= 5;
+    /*else
+        pix -= 5;*/
     el.style.marginLeft = pix +'px';
 }
 el.onClick = function() {
