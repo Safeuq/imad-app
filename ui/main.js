c5815a29fd682ace22a16c619f5ac442139f6c6e
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-26 20:01:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var el = document.getElementById('madi');
 var pix= 0;
 var state=true;
 var move =false;
+var pic = "https://scontent.fmaa2-1.fna.fbcdn.net/v/t1.0-9/15380327_1161699963948097_5793214425944409063_n.jpg?oh=d6563b06fdaa7ea509d07f91e0f532d8&oe=5B4B3FE7";
 function moveIt() {
     if(pix===200)
         state=false;
