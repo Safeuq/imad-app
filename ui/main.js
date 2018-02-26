8a58f3b2e891e797a0b83a6ce06910c49815f92d
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-26 20:22:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,5 +24,5 @@ var button = document.getElementById("counter");
 button.onclick=function() {
     counter = counter + 1;
     var span = document.getElementById("count");
-    span.innerHtml = counter.toString();
+    span.innerHTML = counter.toString();
 };
