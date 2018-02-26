c8f6c8fee70f15cb5cc70e7589a21df82a46f94f
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-26 21:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,5 +22,11 @@ var nam = document.getElementById("name");
 var name = nam.value;
 var submit = document.getElementById("sub");
 submit.onclick = function() {
-    
+    var names =['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>' + names[i] + '</li>'; 
+    }
+    var ul = document.getElementById('lis');
+    ul.innerHTML = list;
 };
