c92627ea720c086cf18330cd4dfb23f7abd590a0
Safeuq <32428157+Safeuq@users.noreply.github.com>
2018-02-26 22:01:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var names = [];
-app.get('/subname/:name',function(req,res){
-    var name = req.params.name;
+app.get('/subname',function(req,res){
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
